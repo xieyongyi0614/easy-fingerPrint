113a7bd7f41be4b9651bdbda3e5fb11f866123cc
xyy <1796709584@qq.com>
2025-01-07 14:24:19 +0800

    feat: 通过WebRTC获取ip地址

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { FingerPrintInfo } from './types';
 import { canvasFingerPrint, getAgentInfo } from './utils';
+import { getRealIpAddress } from './utils/getRealIpAddress';
 
-export const generateFingerprint = async (): Promise<FingerPrintInfo> => {
-    const fp = await canvasFingerPrint();
-    return {
-        ...fp,
-        ...getAgentInfo(),
-        visitorId: fp.hash,
-        ip: ''
-    };
+export const generateFingerprint = (): Promise<FingerPrintInfo> => {
+    return new Promise((resolve, reject) => {
+        canvasFingerPrint()
+            .then((fp) =>
+                getRealIpAddress((ip) => resolve({ ...fp, ...getAgentInfo(), visitorId: fp.hash, ip: ip.ipv4 ? ip.ipv4 : ip.ipv6 }))
+            )
+            .catch((err) => reject(err));
+    });
 };
